47995ff1ade1a4caba4ca076d318a42233e2a09f
Mtn-Drew <drewc3@gmail.com>
2021-09-12 12:17:35 -0600

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trav-ang-crash';
+  title: string = 'TravAngCrash Task Tracker';
 }
